a0d722a454fb347ba08ee932ad053a58cd828c8a
yassine <yassinechatti01234@gmail.com>
2024-05-08 19:01:55 +0200

    second

--- controllers/Panier.js
+++ controllers/Panier.js
@@ -4,41 +4,41 @@ const User = require("../models/User");
 
 /**creation du panier */
 
-const addPanier = async (req, res) => {
-  try {
-    const {
-      clientId,
-      articleId,
-      referencePanier,
-      quantity,
-      nameClient,
-      nameArticle,
-    } = req.body;
-    article = await Article.findById(articleId);
-    client = await User.findById(clientId);
-    if (!article) {
-      return res.status(404).send(error);
-    }
-    total = parseFloat(article.price) * quantity;
-    nameA = article.name;
-    nameC = client.name;
-    const panier = new Panier({
-      referencePanier,
-      clientId,
-      articleId,
-      quantity,
-      Total: total,
-      nameClient: nameC,
-      nameArticle: nameA,
-    });
-    savedPanier = await panier.save();
-    res.status(200).send(savedPanier);
-  } catch (error) {
-    console.log(error);
-    console.log("error while creating the panier");
-    res.status(400).send(error);
-  }
-};
+// const addPanier = async (req, res) => {
+//   try {
+//     const {
+//       clientId,
+//       articleId,
+//       referencePanier,
+//       quantity,
+//       nameClient,
+//       nameArticle,
+//     } = req.body;
+//     article = await Article.findById(articleId);
+//     client = await User.findById(clientId);
+//     if (!article) {
+//       return res.status(404).send(error);
+//     }
+//     total = parseFloat(article.price) * quantity;
+//     nameA = article.name;
+//     nameC = client.name;
+//     const panier = new Panier({
+//       referencePanier,
+//       clientId,
+//       articleId,
+//       quantity,
+//       Total: total,
+//       nameClient: nameC,
+//       nameArticle: nameA,
+//     });
+//     savedPanier = await panier.save();
+//     res.status(200).send(savedPanier);
+//   } catch (error) {
+//     console.log(error);
+//     console.log("error while creating the panier");
+//     res.status(400).send(error);
+//   }
+// };
 
 /**afficher du panier */
 const getPanier = async (req, res) => {
@@ -48,6 +48,52 @@ const getPanier = async (req, res) => {
   } catch (error) {
     console.log(error);
     console.log("error while getting panier");
+  }
+};
+
+const addPanier = async (req, res) => {
+  try {
+    const { clientId, referencePanier, items } = req.body;
+    const client = await User.findById(clientId);
+
+    if (!client) {
+      return res.status(404).send({ error: "Client not found" });
+    }
+
+    let totalPanier = 0;
+    const itemsProcessed = await Promise.all(
+      items.map(async (item) => {
+        const article = await Article.findById(item.articleId);
+        if (!article) {
+          throw new Error(`Article with ID ${item.articleId} not found`);
+        }
+
+        const total =
+          parseFloat(article.price.replace("DT", "")) * item.quantity;
+        totalPanier += total;
+
+        return {
+          articleId: item.articleId,
+          quantity: item.quantity,
+          total,
+          nameArticle: article.name,
+        };
+      })
+    );
+
+    const panier = new Panier({
+      referencePanier,
+      clientId,
+      items: itemsProcessed,
+      totalPanier,
+      nameClient: client.name,
+    });
+
+    const savedPanier = await panier.save();
+    res.status(200).send(savedPanier);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ error: "Error while creating the panier" });
   }
 };
 
--- models/Panier.js
+++ models/Panier.js
@@ -1,33 +1,80 @@
+// const mongoose = require("mongoose");
+
+// const panierSchema = mongoose.Schema({
+//   referencePanier: {
+//     type: String,
+//     required: [true, "panier must have a reference"],
+//   },
+//   clientId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "User",
+//     required: [true, "Client missing!!!"],
+//   },
+//   articleId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Article",
+//     required: [true, "Article missing!!!"],
+//   },
+//   quantity: {
+//     type: String,
+//     required: [true, "Quantity missing!!!"],
+//   },
+//   Total: {
+//     type: String,
+//     required: [true, "price missing!!!"],
+//   },
+//   nameClient: {
+//     type: String,
+//   },
+//   nameArticle: {
+//     type: String,
+//   },
+// });
+
+// const Panier = mongoose.model("Panier", panierSchema);
+
+// module.exports = Panier;
+
 const mongoose = require("mongoose");
+
+const itemSchema = mongoose.Schema({
+  articleId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Article",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  nameArticle: {
+    type: String,
+    required: true,
+  },
+});
 
 const panierSchema = mongoose.Schema({
   referencePanier: {
     type: String,
-    required: [true, "panier must have a reference"],
+    required: true,
   },
   clientId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: [true, "Client missing!!!"],
+    required: true,
   },
-  articleId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Article",
-    required: [true, "Article missing!!!"],
-  },
-  quantity: {
-    type: String,
-    required: [true, "Quantity missing!!!"],
-  },
-  Total: {
-    type: String,
-    required: [true, "price missing!!!"],
+  items: [itemSchema],
+  totalPanier: {
+    type: Number,
+    required: true,
   },
   nameClient: {
     type: String,
-  },
-  nameArticle: {
-    type: String,
+    required: true,
   },
 });
 
